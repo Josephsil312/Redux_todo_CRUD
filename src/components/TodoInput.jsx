0e939d4ad4febbcf259f9bb1065ca25824e30d90
Joseph silvester stallone R <67551732+Josephsil312@users.noreply.github.com>
2021-05-24 10:19:56 +0530

    Updated Button from Material UI

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -4,22 +4,26 @@ import { v1 as uuid } from "uuid";
 import { useDispatch } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
 import { TextField, Grid } from "@material-ui/core/";
+import Button from '@material-ui/core/Button';
 
 function TodoInput() {
   let [name, setName] = useState();
   let dispatch = useDispatch();
   return (
-    <div>
+      <Paper elevation={10}>
       <Grid container spacing={3}>
         <Grid item xs={6}>
           <TextField
+            placeholder = 'Enter what you wanna do'
             label="Todo Name"
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
         </Grid>
         <Grid item xs={6}>
-          <button
+          <Button
+            variant="contained"
+            color="primary"
             onClick={() => {
               dispatch(
                 addTodo({
@@ -29,13 +33,13 @@ function TodoInput() {
               );
               setName("");
             }}
-            className="btn btn-primary mx-2"
+          
           >
             Add Todo
-          </button>
+          </Button>
         </Grid>
       </Grid>
-    </div>
+    </Paper>
   );
 }
 
