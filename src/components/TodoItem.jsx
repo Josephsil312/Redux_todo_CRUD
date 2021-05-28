6b0f82812ba813cfad2badbf691274a94dabd7a3
Joseph silvester stallone R <67551732+Josephsil312@users.noreply.github.com>
2021-05-28 12:43:22 +0530

    Update TodoItem.jsx

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,28 +3,18 @@ import { useDispatch } from "react-redux";
 import { deleteTodo, updateTodo } from "../redux/actions";
 import { IconButton, Grid, TextField, Paper } from "@material-ui/core/";
 import { Delete, Edit, Save } from "@material-ui/icons/";
-import Container from '@material-ui/core/Container';
-import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
-  root: {
-    marginTop: 16,
-    marginBottom: 5,
-    padding: 16,
-    boxShadow: "0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)"
-  },
 
-});
 
 function TodoItem({ todo, index }) {
  
-  const classes = useStyles();
+ 
   const [editable, setEditable] = useState(false);
   const [name, setName] = useState(todo.name);
   let dispatch = useDispatch();
 
   return (
-    <Container maxWidth="sm" className={classes.root}>
+   
     
     <Grid container direction="row">
       
@@ -66,7 +56,7 @@ function TodoItem({ todo, index }) {
         </IconButton>
       </Grid>
     </Grid>
-   </Container>
+   
   );
 }
 
